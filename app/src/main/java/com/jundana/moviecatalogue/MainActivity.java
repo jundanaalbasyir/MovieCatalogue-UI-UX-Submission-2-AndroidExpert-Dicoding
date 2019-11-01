2cf2bbd594354f38f16e9f194aae413223aa0320
Jundana <jalbasyir@gmail.com>
2019-11-01 10:12:51 +0700

    Localization ID-EN / EN-ID

--- app/src/main/java/com/jundana/moviecatalogue/MainActivity.java
+++ app/src/main/java/com/jundana/moviecatalogue/MainActivity.java
@@ -1,15 +1,18 @@
 package com.jundana.moviecatalogue;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.text.Html;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 import com.jundana.moviecatalogue.adapter.SectionsPagerAdapter;
+
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +28,20 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setTabTextColors(Color.parseColor("#000000"), Color.parseColor("#000000"));
         tabLayout.setupWithViewPager(viewPager);
         Objects.requireNonNull(getSupportActionBar()).setElevation(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_language) {
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
